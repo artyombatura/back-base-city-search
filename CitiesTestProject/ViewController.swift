b74157d926d7e691ad2b0593e5d2dfca9d324c66
Artsiom Batura <Artsiom.Batura@itechart-group.com>
2022-04-06 09:26:58 +0300

    Initial commit. Project created.

---
+++ CitiesTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CitiesTestProject
+//
+//  Created by Artyom Batura on 6.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
